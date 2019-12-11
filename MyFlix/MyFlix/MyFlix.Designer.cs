8f89d2b5d74d088d1bdbc1dcf48b171c2be3d92e
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-11 08:49:21 -0600

    Update MyFlix.Designer.cs

--- MyFlix/MyFlix/MyFlix.Designer.cs
+++ MyFlix/MyFlix/MyFlix.Designer.cs
@@ -28,20 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lstSelections = new System.Windows.Forms.ListBox();
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.btnFinished = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // lstSelections
+            // 
+            this.lstSelections.FormattingEnabled = true;
+            this.lstSelections.Items.AddRange(new object[] {
+            "Movie One",
+            "Movie Two",
+            "Movie Three",
+            "Movie Four",
+            "Movie Five",
+            "Movie Six"});
+            this.lstSelections.Location = new System.Drawing.Point(12, 12);
+            this.lstSelections.Name = "lstSelections";
+            this.lstSelections.Size = new System.Drawing.Size(120, 82);
+            this.lstSelections.TabIndex = 0;
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Location = new System.Drawing.Point(139, 13);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(287, 13);
+            this.lblInstructions.TabIndex = 1;
+            this.lblInstructions.Text = "Choose as many downloads as you want by holding ctrl key";
+            // 
+            // btnFinished
+            // 
+            this.btnFinished.Location = new System.Drawing.Point(142, 70);
+            this.btnFinished.Name = "btnFinished";
+            this.btnFinished.Size = new System.Drawing.Size(75, 23);
+            this.btnFinished.TabIndex = 2;
+            this.btnFinished.Text = "Finish";
+            this.btnFinished.UseVisualStyleBackColor = true;
+            // 
+            // MyFlix
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
+            this.Controls.Add(this.btnFinished);
+            this.Controls.Add(this.lblInstructions);
+            this.Controls.Add(this.lstSelections);
+            this.Name = "MyFlix";
             this.Text = "MyFlix";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lstSelections;
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.Button btnFinished;
     }
 }
 
