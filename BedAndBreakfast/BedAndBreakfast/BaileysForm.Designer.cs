c68ba5b500dbe8ef055eb77eef4571017c317bc0
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-11 08:15:03 -0600

    created welcomeLabel

--- BedAndBreakfast/BedAndBreakfast/BaileysForm.Designer.cs
+++ BedAndBreakfast/BedAndBreakfast/BaileysForm.Designer.cs
@@ -28,20 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.welcomeLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // welcomeLabel
+            // 
+            this.welcomeLabel.AutoSize = true;
+            this.welcomeLabel.Location = new System.Drawing.Point(60, 30);
+            this.welcomeLabel.Name = "welcomeLabel";
+            this.welcomeLabel.Size = new System.Drawing.Size(102, 13);
+            this.welcomeLabel.TabIndex = 0;
+            this.welcomeLabel.Text = "Welcome to Bailey\'s";
             // 
             // BaileysForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.welcomeLabel);
             this.Name = "BaileysForm";
             this.Text = "Bailey\'s Bed and Breakfast";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label welcomeLabel;
     }
 }
 
