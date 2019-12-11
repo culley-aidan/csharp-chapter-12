b3ccca3ae070e5917072fb741a6a3a353eff65df
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-11 08:44:36 -0600

    done

--- FiveColors2/FiveColors2/FiveColors2.Designer.cs
+++ FiveColors2/FiveColors2/FiveColors2.Designer.cs
@@ -40,55 +40,60 @@
             this.radColorOne.AutoSize = true;
             this.radColorOne.Location = new System.Drawing.Point(13, 13);
             this.radColorOne.Name = "radColorOne";
-            this.radColorOne.Size = new System.Drawing.Size(85, 17);
+            this.radColorOne.Size = new System.Drawing.Size(46, 17);
             this.radColorOne.TabIndex = 0;
             this.radColorOne.TabStop = true;
-            this.radColorOne.Text = "radioButton1";
+            this.radColorOne.Text = "Blue";
             this.radColorOne.UseVisualStyleBackColor = true;
+            this.radColorOne.CheckedChanged += new System.EventHandler(this.radColorOne_CheckedChanged);
             // 
             // radColorTwo
             // 
             this.radColorTwo.AutoSize = true;
             this.radColorTwo.Location = new System.Drawing.Point(13, 37);
             this.radColorTwo.Name = "radColorTwo";
-            this.radColorTwo.Size = new System.Drawing.Size(85, 17);
+            this.radColorTwo.Size = new System.Drawing.Size(52, 17);
             this.radColorTwo.TabIndex = 1;
             this.radColorTwo.TabStop = true;
-            this.radColorTwo.Text = "radioButton1";
+            this.radColorTwo.Text = "Black";
             this.radColorTwo.UseVisualStyleBackColor = true;
+            this.radColorTwo.CheckedChanged += new System.EventHandler(this.radColorTwo_CheckedChanged);
             // 
             // radColorThree
             // 
             this.radColorThree.AutoSize = true;
             this.radColorThree.Location = new System.Drawing.Point(13, 61);
             this.radColorThree.Name = "radColorThree";
-            this.radColorThree.Size = new System.Drawing.Size(85, 17);
+            this.radColorThree.Size = new System.Drawing.Size(45, 17);
             this.radColorThree.TabIndex = 2;
             this.radColorThree.TabStop = true;
-            this.radColorThree.Text = "radioButton1";
+            this.radColorThree.Text = "Red";
             this.radColorThree.UseVisualStyleBackColor = true;
+            this.radColorThree.CheckedChanged += new System.EventHandler(this.radColorThree_CheckedChanged);
             // 
             // radColorFour
             // 
             this.radColorFour.AutoSize = true;
             this.radColorFour.Location = new System.Drawing.Point(13, 85);
             this.radColorFour.Name = "radColorFour";
-            this.radColorFour.Size = new System.Drawing.Size(85, 17);
+            this.radColorFour.Size = new System.Drawing.Size(55, 17);
             this.radColorFour.TabIndex = 3;
             this.radColorFour.TabStop = true;
-            this.radColorFour.Text = "radioButton1";
+            this.radColorFour.Text = "Brown";
             this.radColorFour.UseVisualStyleBackColor = true;
+            this.radColorFour.CheckedChanged += new System.EventHandler(this.radColorFour_CheckedChanged);
             // 
             // radColorFive
             // 
             this.radColorFive.AutoSize = true;
             this.radColorFive.Location = new System.Drawing.Point(13, 109);
             this.radColorFive.Name = "radColorFive";
-            this.radColorFive.Size = new System.Drawing.Size(85, 17);
+            this.radColorFive.Size = new System.Drawing.Size(53, 17);
             this.radColorFive.TabIndex = 4;
             this.radColorFive.TabStop = true;
-            this.radColorFive.Text = "radioButton1";
+            this.radColorFive.Text = "White";
             this.radColorFive.UseVisualStyleBackColor = true;
+            this.radColorFive.CheckedChanged += new System.EventHandler(this.radColorFive_CheckedChanged);
             // 
             // FiveColors2
             // 
--- FiveColors2/FiveColors2/FiveColors2.cs
+++ FiveColors2/FiveColors2/FiveColors2.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace FiveColors2
@@ -15,6 +9,47 @@ namespace FiveColors2
         public FiveColors2()
         {
             InitializeComponent();
+        }
+
+        private void radColorOne_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radColorOne.Checked) {
+                ActiveForm.BackColor = Color.Blue;
+            } else {
+                ActiveForm.BackColor = DefaultBackColor;
+            }
+        }
+        private void radColorTwo_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radColorTwo.Checked) {
+                ActiveForm.BackColor = Color.Black;
+            } else {
+                ActiveForm.BackColor = DefaultBackColor;
+            }
+        }
+        private void radColorThree_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radColorThree.Checked) {
+                ActiveForm.BackColor = Color.Red;
+            } else {
+                ActiveForm.BackColor = DefaultBackColor;
+            }
+        }
+        private void radColorFour_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radColorFour.Checked) {
+                ActiveForm.BackColor = Color.Brown;
+            } else {
+                ActiveForm.BackColor = DefaultBackColor;
+            }
+        }
+        private void radColorFive_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radColorFive.Checked) {
+                ActiveForm.BackColor = Color.White;
+            } else {
+                ActiveForm.BackColor = DefaultBackColor;
+            }
         }
     }
 }
