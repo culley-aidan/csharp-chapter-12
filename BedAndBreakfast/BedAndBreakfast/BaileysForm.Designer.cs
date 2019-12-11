c0a0623c1a8142b93134d4cc12e3252f8562a532
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-11 08:16:37 -0600

    created rateLabel

--- BedAndBreakfast/BedAndBreakfast/BaileysForm.Designer.cs
+++ BedAndBreakfast/BedAndBreakfast/BaileysForm.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.welcomeLabel = new System.Windows.Forms.Label();
+            this.rateLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // welcomeLabel
@@ -41,11 +42,22 @@
             this.welcomeLabel.TabIndex = 0;
             this.welcomeLabel.Text = "Welcome to Bailey\'s";
             // 
+            // rateLabel
+            // 
+            this.rateLabel.AutoSize = true;
+            this.rateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rateLabel.Location = new System.Drawing.Point(80, 80);
+            this.rateLabel.Name = "rateLabel";
+            this.rateLabel.Size = new System.Drawing.Size(121, 20);
+            this.rateLabel.TabIndex = 1;
+            this.rateLabel.Text = "Check our rates";
+            // 
             // BaileysForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rateLabel);
             this.Controls.Add(this.welcomeLabel);
             this.Name = "BaileysForm";
             this.Text = "Bailey\'s Bed and Breakfast";
@@ -57,6 +69,7 @@
         #endregion
 
         private System.Windows.Forms.Label welcomeLabel;
+        private System.Windows.Forms.Label rateLabel;
     }
 }
 
